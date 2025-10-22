3c01e2e2ed2be4f9b2f557fb210201d08a61dbb9
AdsFixter Devloper <dev.adsdixter@gmail.com>
2025-10-22 12:42:49 +0600

    option asection

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Services from "./components/Services";
 import ConsultancyForm from "./components/Consultation";
 import BenefitsSection from "./components/BenefitsSection";
 import LatestBlogsSection from "./components/LatestBlogsSection";
+import Option from "./components/Option";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <ConsultancyForm></ConsultancyForm>
       <BenefitsSection></BenefitsSection>
       <LatestBlogsSection></LatestBlogsSection>
+      <Option></Option>
     </div>
   );
 }
