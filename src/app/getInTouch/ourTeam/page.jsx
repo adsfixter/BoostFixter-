f6d2ad899a019804c817b7a3cc3d31068b584c80
AdsFixter Devloper <dev.adsdixter@gmail.com>
2025-10-22 17:38:30 +0600

    Save local changes before pull

---
+++ src/app/getInTouch/ourTeam/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OurTeam() {
+  return (
+    <div>
+        <h1>our team</h1>
+    </div>
+  )
+}
---
+++ src/app/getInTouch/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GetInTouch() {
+  return (
+    <div>
+      <h1>getintouch</h1>
+    </div>
+  )
+}
