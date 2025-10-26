44559c10ffbf9254fbb63f7f8fcdac8f7666007a
AdsFixter Devloper <adsfixterdevloper@AdsFixters-Mac-mini.local>
2025-10-26 18:56:50 +0600

    test pull

--- src/app/components/services/WebDevelopment/ThrivesDevelopment.jsx
+++ src/app/components/services/WebDevelopment/ThrivesDevelopment.jsx
@@ -4,7 +4,7 @@ export default function ThrivesDevelopment() {
   return (
       <div className="bg-gray-50 px-6 py-20 md:px-12 md:py-32">
           <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-              
+              <h1>thi si. ksjdj</h1>
           </div>
     </div>
   )
