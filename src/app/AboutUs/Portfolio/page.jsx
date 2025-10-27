23f4de09d04021391192631612ad324fb46c9486
AdsFixter Devloper <dev.adsdixter@gmail.com>
2025-10-27 10:53:03 +0600

    portfolio component added

--- src/app/AboutUs/Portfolio/page.jsx
+++ src/app/AboutUs/Portfolio/page.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
-export default function Portfolio() {
+
+import BusinessProposes from "@/app/components/portfolio/BusinessProposes";
+import ContentSection from "@/app/components/portfolio/contentSection";
+import GraphicsDeliver from "@/app/components/portfolio/GraphicsDeliver";
+import PortfolioHomePage from "@/app/components/portfolio/portfolioHomePage/home";
+import PortfolioVideo from "@/app/components/portfolio/PortfolioVideo";
+import SEOProjects from "@/app/components/portfolio/SEOProjects";
+import SomeExplainerVideo from "@/app/components/portfolio/SomeExplainerVideo";
+import VoiceOver from "@/app/components/portfolio/VoiceOver";
+import WebApplications from "@/app/components/portfolio/WebApplications";
+
+export default function portfolio() {
   return (
-    <div>Portfolio</div>
+    <div>
+      <PortfolioHomePage></PortfolioHomePage>
+      <ContentSection></ContentSection>
+      <PortfolioVideo></PortfolioVideo>
+      <SomeExplainerVideo></SomeExplainerVideo>
+      <GraphicsDeliver></GraphicsDeliver>
+      <WebApplications></WebApplications>
+      <SEOProjects></SEOProjects>
+      <VoiceOver></VoiceOver>
+      <BusinessProposes></BusinessProposes>
+      
+     
+    </div>
   )
 }
