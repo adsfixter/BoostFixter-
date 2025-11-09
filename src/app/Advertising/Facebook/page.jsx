9b0b54632960741f56bd0aeddde12f053323b093
AdsFixter Devloper <adsfixterdevloper@AdsFixters-Mac-mini.local>
2025-11-09 16:56:07 +0600

    facebook page what we do section added

--- src/app/Advertising/Facebook/page.jsx
+++ src/app/Advertising/Facebook/page.jsx
@@ -1,12 +1,16 @@
 
 import FacebookAds from '@/app/components/advertising/Facebook/FacebookAds'
+import Pricing from '@/app/components/advertising/Facebook/Pricing'
+import WhatWeDo from '@/app/components/advertising/Facebook/WhatWeDo'
 import React from 'react'
 
 export default function Facebook() {
   return (
    
     <div>
-     <FacebookAds></FacebookAds>
+      <FacebookAds></FacebookAds>
+      <WhatWeDo></WhatWeDo>
+      {/* <Pricing></Pricing> */}
     </div>
   )
 }
